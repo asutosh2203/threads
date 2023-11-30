f8bd55fde9a90dc6fa498c2c4221c30ee50fe788
Asutosh Padhi <asutosh2001@gmail.com>
2023-12-01 02:22:43 +0530

    Authentication added

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        Onboarding page
+    </div>
+  )
+}
+
+export default Page
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Treads
+      Hello
+      <UserButton afterSignOutUrl="/onboarding"/>
     </main>
   )
 }
